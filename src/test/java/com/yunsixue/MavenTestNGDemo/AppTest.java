b6dd947117652e2eba3d9a0e4b962eefa5e49da3
chentaxing <ing.a.t@163.com>
2018-04-01 15:24:39 +0800

    the test of github

---
+++ src/test/java/com/yunsixue/MavenTestNGDemo/AppTest.java
@@ -0,0 +1,12 @@
+package com.yunsixue.MavenTestNGDemo;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@Test
+    public void mavenTest(){
+      System.out.println("firstTest");
+    }
+
+}
